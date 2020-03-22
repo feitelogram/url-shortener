9327deb70f5d9cd42041b86db3403499e60abeb0
Nicholas Feitel <Nicholas.Feitel@gmail.com>
2020-03-22 13:29:36 -0400

    python proj

---
+++ urlshort/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__) 
+    app.secret_key = 'flask_is_silly'
+
+    from . import urlshort
+    app.register_blueprint(urlshort.bp)
+
+    return app
